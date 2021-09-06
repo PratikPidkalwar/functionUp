aabc3c81767b42fcc7a9926aceb876faf6d80f85
mukulgadd <mukulgaddhyan@gmail.com>
2021-09-06 14:37:03 +0530

    qq q `Please enter the commit message for your changes. Lines starting

--- firstproject/src/Routes/index.js
+++ firstproject/src/Routes/index.js
@@ -6,5 +6,17 @@ const solve = require('../Exercises/exercise.js');
 
 const router = express.Router();
 
+const practiceData = [[1,2,3,4], [3,4,5,6], [5,6,7,8], [7,8,9,10], [9,10,11,12]]
+
+
+for(let i=0;i<practiceData.length;i++)
+{
+  for(let j=0;j<4;j++)
+  {
+    practiceData[i][j]=++practiceData[i][j];
+  }
+}
+return practiceData;
+
 
 module.exports = router;
